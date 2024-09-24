445c68fe128ff7241f4bcee1879e4110db16abc4
Vlad Romanov <vladromeur@gmail.com>
2024-09-24 19:50:48 +0500

    Добавил тему по умолчанию для Angular Material

--- src/app/components/city-selector/city-selector.component.ts
+++ src/app/components/city-selector/city-selector.component.ts
@@ -4,12 +4,11 @@ import {RouterLink} from "@angular/router";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
 import { FormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
   selector: 'app-city-selector',
   standalone: true,
-  imports: [RouterLink, MatFormFieldModule, MatSelectModule, FormsModule, BrowserAnimationsModule],
+  imports: [RouterLink, MatFormFieldModule, MatSelectModule, FormsModule,],
   templateUrl: './city-selector.component.html',
   styleUrl: './city-selector.component.scss'
 })
