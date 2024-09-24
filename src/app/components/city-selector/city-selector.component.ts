1fedab0a5c4e5bcf11d8af7aea7123284d5f4ce4
Vlad Romanov <vladromeur@gmail.com>
2024-09-24 21:12:55 +0500

    Добавил интрефейс для города, добавил browserAnimationModule

--- src/app/components/city-selector/city-selector.component.ts
+++ src/app/components/city-selector/city-selector.component.ts
@@ -4,6 +4,9 @@ import {RouterLink} from "@angular/router";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
 import { FormsModule } from "@angular/forms";
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { City } from "../../types/interfaces/city.interface";
 
 @Component({
   selector: 'app-city-selector',
@@ -13,5 +16,8 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './city-selector.component.scss'
 })
 export class CitySelectorComponent {
+  private readonly cities: City[] = [{id: 1, name: 'Astana'}, {id: 2, name: 'Almaty'}]
+
+  public selectedCity: City = this.cities[1]
 
 }
