8333705e6714fc6fc2123853128b931860e08c7e
Vlad Romanov <vladromeur@gmail.com>
2024-09-25 21:46:01 +0500

    Создал компонент для выбора теннисного центра

---
+++ src/app/components/tennis-center-selection/tennis-center-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tennis-center-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './tennis-center-selection.component.html',
+  styleUrl: './tennis-center-selection.component.scss'
+})
+export class TennisCenterSelectionComponent {
+
+}
