05f363cf2863e94cab3a2d94a8944c7363a6067d
Vlad Romanov <vladromeur@gmail.com>
2024-09-29 20:50:28 +0500

    Создал компонент court-filter

---
+++ src/app/components/court-filter/court-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-court-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './court-filter.component.html',
+  styleUrl: './court-filter.component.scss'
+})
+export class CourtFilterComponent {
+
+}
